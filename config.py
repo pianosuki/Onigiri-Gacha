b581aed06f2b20a34f314ed6b4e5e7cc6a825502
pianosuki <81691667+pianosuki@users.noreply.github.com>
2022-08-30 16:27:40 -0700

    rebalance weights

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ consumables             = {
 weights         = {"tier_1": [25, 25, 20, 15, 10, 5], # Probability % out of 100 for each capsule respectively. (Must add up to 100 total!)
                    "tier_2": [25, 25, 20, 15, 10, 5],
                    "tier_3": [25, 25, 20, 15, 10, 5],
-                   "tier_4": [40, 32, 10, 8, 5, 5]}
+                   "tier_4": [50, 30, 10, 3, 2, 5]}
 weight_mods     = [0] # Applied respectively according to how many times prize has been rolled. 0 rolled = weight_mods[0], 1 rolled = weight_mods[1], etc.
 encouragement   = {"tier_1": [32, 25, 18, 12, 8, 5],
                    "tier_2": [32, 25, 18, 12, 8, 5],
